39a2701588474d5923e9c219407c24a83c510739
iAlex0 <suavi010@gmail.com>
2023-03-23 17:27:44 -0700

    initial

---
+++ src/App.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { Container, Box } from '@chakra-ui/react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import TextInput from './components/TextInput';
+import KeywordsModal from './components/KeywordsModal';
+
+const App = () => {
+
+  const [keywords, setKeywords] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const extractKeywords = async (text) => {
+    setLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt:
+          'Extract keywords from this text. Make the first letter of every word uppercase and separate with commas:\n\n' +
+          text +
+          '',
+        temperature: 0.5,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.8,
+        presence_penalty: 0.0,
+      }),
+    };
+
+    const response = await fetch(import.meta.env.VITE_OPENAI_API_URL, options);
+
+    const json = await response.json();
+
+    const data = json.choices[0].text.trim();
+
+    // console.log(data);
+    setKeywords(data);
+    setLoading(false);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+  
+  return (
+    <Box bg='blue.600' color='white' height='100vh' width='100vw' paddingTop={100}>
+      <Container maxW='3xl' centerContent>
+          <Header/>
+          <TextInput extractKeywords={extractKeywords} />
+          <Footer height={30}/>
+      </Container>
+      <KeywordsModal 
+        keywords={keywords} 
+        loading={loading} 
+        isOpen={isOpen} 
+        closeModal={closeModal}
+      />
+    </Box>
+  );
+};
+
+export default App;
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,49 @@
+import { Textarea, Button, useToast } from '@chakra-ui/react'
+import { useState } from 'react'
+
+const TextInput = ({extractKeywords }) => {
+    const [text, setText] = useState('')
+
+    const toast = useToast()
+
+    const submitText = () => {
+        if (text === '') {
+            toast({
+                title: 'No text found',
+                description: 'Please paste your text in the text area',
+                status: 'error',
+                duration: 5000,
+                isClosable: true
+            });
+        } else {
+            extractKeywords(text)
+        }
+    };
+   
+    return (
+        <>
+        <Textarea
+            bg='blue.400'
+            color='white'
+            padding={4}
+            marginTop={6}
+            height={200}
+            placeholder='Paste your text here'
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+        />
+        <Button
+            bg='blue.500'
+            color='white'
+            marginTop={4}
+            width='100%'
+            _hover={{ bg: 'blue.700'}}
+            onClick={submitText}
+        >
+            Extract Keywords
+        </Button>
+    </>
+    )
+}
+
+export default TextInput
